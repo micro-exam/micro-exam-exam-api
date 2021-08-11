c6deb1947ec1188f5a537a6869569dc95770f432
Sohan Patel <sohanpatel96@gmail.com>
2021-08-11 12:32:54 -0400

    intial project

---
+++ src/main/java/com/microexam/examapi/ExamApiApplication.java
@@ -0,0 +1,13 @@
+package com.microexam.examapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microexam/examapi/ExamApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microexam.examapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
