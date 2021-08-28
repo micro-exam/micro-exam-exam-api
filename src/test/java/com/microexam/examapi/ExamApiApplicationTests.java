b0abb9afa7b43ce93fe301331c14a72cd7e0125d
Sohan Patel <sohanpatel96@gmail.com>
2021-08-28 15:40:29 -0400

    added santiy test

--- src/test/java/com/microexam/examapi/ExamApiApplicationTests.java
+++ src/test/java/com/microexam/examapi/ExamApiApplicationTests.java
@@ -1,13 +1,21 @@
 package com.microexam.examapi;
 
+import com.microexam.examapi.controller.ExamController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class ExamApiApplicationTests {
 
+	@Autowired
+	ExamController examController;
+
+
 	@Test
 	void contextLoads() {
+		assertThat(examController).isNotNull();
 	}
 
 }
